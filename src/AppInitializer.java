f07c44973929f6e43dacfb7dfc805d57cbc5dc99
Kavithma Thushal <kavithmathushal9007@gmai.com>
2023-08-24 22:21:19 +0530

    Project Structure Implemented

---
+++ src/AppInitializer.java
@@ -0,0 +1,19 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * @author : Kavithma Thushal
+ * @project : Hostel-Management-System
+ * @since : 10:05 PM - 8/24/2023
+ **/
+public class AppInitializer extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
