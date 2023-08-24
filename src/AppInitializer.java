01447cbfc331a21296f55c4abc260c35e6753560
Kavithma Thushal <kavithmathushal9007@gmai.com>
2023-08-24 22:47:01 +0530

    Login Implementing

--- src/AppInitializer.java
+++ src/AppInitializer.java
@@ -1,5 +1,10 @@
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import java.io.IOException;
 
 /**
  * @author : Kavithma Thushal
@@ -14,6 +19,14 @@ public class AppInitializer extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-
+        try {
+            primaryStage.setScene(new Scene(FXMLLoader.load(getClass().getResource("lk/ijse/gdse66/hostel/view/login_form.fxml"))));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        primaryStage.setTitle("login");
+        primaryStage.setResizable(false);
+        //primaryStage.initStyle(StageStyle.UNDECORATED);
+        primaryStage.show();
     }
 }
---
+++ src/lk/ijse/gdse66/hostel/controller/LoginFormController.java
@@ -0,0 +1,10 @@
+package lk.ijse.gdse66.hostel.controller;
+
+/**
+ * @author : Kavithma Thushal
+ * @project : Hostel-Management-System
+ * @since : 10:41 PM - 8/24/2023
+ **/
+public class LoginFormController {
+
+}
