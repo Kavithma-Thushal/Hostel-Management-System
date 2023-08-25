1efd6bc8168f1fd093dd8afe66ed7b958ce9b671
Kavithma Thushal <kavithmathushal9007@gmai.com>
2023-08-25 15:35:38 +0530

    Manage KeyMoney UI Implementing

--- src/lk/ijse/gdse66/hostel/controller/ManageKeymoneyFormController.java
+++ src/lk/ijse/gdse66/hostel/controller/ManageKeymoneyFormController.java
@@ -1,5 +1,15 @@
 package lk.ijse.gdse66.hostel.controller;
 
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 /**
  * @author : Kavithma Thushal
  * @project : Hostel-Management-System
@@ -7,4 +17,24 @@ package lk.ijse.gdse66.hostel.controller;
  **/
 public class ManageKeymoneyFormController {
 
+    @FXML
+    private AnchorPane root;
+    @FXML
+    private JFXTextField txtSearch;
+
+    @FXML
+    private void searchOnAction(ActionEvent actionEvent) {
+
+    }
+
+    @FXML
+    private void backOnAction(ActionEvent actionEvent) {
+        Stage stage = (Stage) this.root.getScene().getWindow();
+        try {
+            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/lk/ijse/gdse66/hostel/view/dashboard_form.fxml"))));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        stage.setTitle("Dashboard");
+    }
 }
