9eb5f01b370bd3dc86e6b37e0207b989ae51b7b1
Kavithma Thushal <kavithmathushal9007@gmai.com>
2023-08-27 14:01:34 +0530

    Dashboard UI Refactored

--- src/lk/ijse/gdse66/hostel/controller/DashboardFormController.java
+++ src/lk/ijse/gdse66/hostel/controller/DashboardFormController.java
@@ -3,9 +3,8 @@ package lk.ijse.gdse66.hostel.controller;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
-import javafx.stage.Stage;
 
 import java.io.IOException;
 
@@ -18,41 +17,45 @@ public class DashboardFormController {
 
     @FXML
     private AnchorPane root;
+    @FXML
+    private Label lblHeader;
+    @FXML
+    private Label lblDate;
+    @FXML
+    private Label lblTime;
 
     @FXML
     private void studentsOnAction(ActionEvent actionEvent) throws IOException {
         root.getChildren().clear();
         root.getChildren().add(FXMLLoader.load(getClass().getResource("/lk/ijse/gdse66/hostel/view/manage_students_form.fxml")));
-        /*Stage stage = (Stage) this.root.getScene().getWindow();
-        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/lk/ijse/gdse66/hostel/view/manage_students_form.fxml"))));
-        stage.setTitle("Manage Students");*/
+        lblHeader.setText("Manage Students");
     }
 
     @FXML
     private void roomsOnAction(ActionEvent actionEvent) throws IOException {
-        Stage stage = (Stage) this.root.getScene().getWindow();
-        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/lk/ijse/gdse66/hostel/view/manage_rooms_form.fxml"))));
-        stage.setTitle("Manage Rooms");
+        root.getChildren().clear();
+        root.getChildren().add(FXMLLoader.load(getClass().getResource("/lk/ijse/gdse66/hostel/view/manage_rooms_form.fxml")));
+        lblHeader.setText("Manage Rooms");
     }
 
     @FXML
     private void reservationOnAction(ActionEvent actionEvent) throws IOException {
-        Stage stage = (Stage) this.root.getScene().getWindow();
-        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/lk/ijse/gdse66/hostel/view/manage_reservation_form.fxml"))));
-        stage.setTitle("Reservation");
+        root.getChildren().clear();
+        root.getChildren().add(FXMLLoader.load(getClass().getResource("/lk/ijse/gdse66/hostel/view/manage_reservation_form.fxml")));
+        lblHeader.setText("Reservation");
     }
 
     @FXML
     private void keyMoneyOnAction(ActionEvent actionEvent) throws IOException {
-        Stage stage = (Stage) this.root.getScene().getWindow();
-        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/lk/ijse/gdse66/hostel/view/manage_keymoney_form.fxml"))));
-        stage.setTitle("Key Money");
+        root.getChildren().clear();
+        root.getChildren().add(FXMLLoader.load(getClass().getResource("/lk/ijse/gdse66/hostel/view/manage_keymoney_form.fxml")));
+        lblHeader.setText("Find Key Money");
     }
 
     @FXML
     private void userOnAction(ActionEvent actionEvent) throws IOException {
-        Stage stage = (Stage) this.root.getScene().getWindow();
-        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/lk/ijse/gdse66/hostel/view/manage_user_form.fxml"))));
-        stage.setTitle("Manage User");
+        root.getChildren().clear();
+        root.getChildren().add(FXMLLoader.load(getClass().getResource("/lk/ijse/gdse66/hostel/view/manage_user_form.fxml")));
+        lblHeader.setText("Manage User");
     }
 }
